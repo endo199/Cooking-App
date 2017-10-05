15763a72eaeca3706138e3e81771223e8aea9e7e
Suhendro <suhendro.mail@gmail.com>
2017-10-05 22:23:36 +0700

    remove state from android manifest

--- app/src/main/java/com/suhendro/cookingtime/DetailInstructionFragment.java
+++ app/src/main/java/com/suhendro/cookingtime/DetailInstructionFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
@@ -42,6 +43,7 @@ import timber.log.Timber;
 public class DetailInstructionFragment extends Fragment {
     public static final String STEP_POSITION = "STEP_POSITION";
     public static final String PLAY_POSITION = "PLAY_POSITION";
+    private static final String WINDOW_INDEX = "WINDOW_INDEX";
 
     @BindView(R.id.vdo_instruction)
     SimpleExoPlayerView mVideo;
@@ -59,16 +61,29 @@ public class DetailInstructionFragment extends Fragment {
 
     private DataSource.Factory datasourceFactory;
     private ExtractorsFactory extractor;
+    private long position;
+    private DefaultTrackSelector trackSelector;
+    private int window;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Timber.d("XXX onCreateView");
         View view = inflater.inflate(R.layout.detail_instruction_fragment, container, false);
 
         unbinder = ButterKnife.bind(this, view);
 
+        initPlayer();
+        window = C.INDEX_UNSET;
+        position = C.TIME_UNSET;
+
+        return view;
+    }
+
+    private void initPlayer() {
+        Timber.d("XXX initPlayer");
         // preparing exo player
-        TrackSelector trackSelector = new DefaultTrackSelector();
+        trackSelector = new DefaultTrackSelector();
         mPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
         mVideo.setPlayer(mPlayer);
 
@@ -76,36 +91,61 @@ public class DetailInstructionFragment extends Fragment {
         TransferListener<? super DataSource> bandwidthMeter = new DefaultBandwidthMeter();
         datasourceFactory = new DefaultDataSourceFactory(getContext(), userAgent, bandwidthMeter);
         extractor = new DefaultExtractorsFactory();
-
-        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Timber.d("XXX onActivityCreated");
         super.onActivityCreated(savedInstanceState);
 
+//        initPlayer();
+
+        Timber.d("XXX there is saved instance state? %s", (savedInstanceState != null));
+
         if(savedInstanceState != null) {
+            position = savedInstanceState.getLong(PLAY_POSITION, C.TIME_UNSET);
+            window = savedInstanceState.getInt(WINDOW_INDEX, C.INDEX_UNSET);
+            Timber.d("XXX last saved position %d", position);
+
             mCurrentStep = savedInstanceState.getInt(STEP_POSITION, 0);
             mListInstruction = (CookingStep[]) savedInstanceState.getParcelableArray("list");
             this.setStep(mCurrentStep);
-
-            long playPosition = savedInstanceState.getLong(PLAY_POSITION, 0);
-            mPlayer.seekTo(playPosition);
         }
     }
 
     @Override
     public void onPause() {
+        Timber.d("XXX onPause");
         super.onPause();
-        mPlayer.release();
+        if(mPlayer != null) {
+            position = mPlayer.getCurrentPosition();
+            window = mPlayer.getCurrentWindowIndex();
+
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        Timber.d("XXX onResume");
+        super.onResume();
+        if(mPlayer == null) {
+            initPlayer();
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
+        Timber.d("XXX onSaveInstanceState");
         super.onSaveInstanceState(outState);
 
-        long playPosition = mPlayer.getCurrentPosition();
-        outState.putLong(PLAY_POSITION, playPosition);
+//        long playPosition = mPlayer.getCurrentPosition();
+        Timber.d("XXX player current position: %d", position);
+
+        outState.putLong(PLAY_POSITION, position);
+        outState.putLong(WINDOW_INDEX, window);
         outState.putInt(STEP_POSITION, mCurrentStep);
 
         outState.putParcelableArray("list", mListInstruction);
@@ -113,11 +153,13 @@ public class DetailInstructionFragment extends Fragment {
 
     @OnClick(R.id.btn_next_instruction)
     public void nextInstruction(View view) {
+        position = C.TIME_UNSET;
         displayInstruction(mCurrentStep + 1);
     }
 
     @OnClick(R.id.btn_prev_instruction)
     public void prevInstruction(View view) {
+        position = C.TIME_UNSET;
         displayInstruction(mCurrentStep - 1);
     }
 
@@ -149,10 +191,15 @@ public class DetailInstructionFragment extends Fragment {
     }
 
     protected void displayVideo(Uri uri) {
+        Timber.d("XXX display video %s", uri.toString());
         MediaSource mediaSource = new ExtractorMediaSource(uri, datasourceFactory, extractor, null, null);
-        mPlayer.prepare(mediaSource);
-        mPlayer.setPlayWhenReady(true);
 
+        boolean isResume = window != C.INDEX_UNSET;
+
+        mPlayer.prepare(mediaSource, !isResume, false);
+        Timber.d("XXX seek to position %d", position);
+        mPlayer.seekTo(position);
+        mPlayer.setPlayWhenReady(true);
     }
 
     protected void displayInstruction(int step) {
